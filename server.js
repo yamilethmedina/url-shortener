2c3f59e2b317ad5514dd360056cc661273c12491
Yami Medina <yamilethmedina1@gmail.com>
2017-01-27 15:16:43 -0500

    shortURLs are created and inserted into database

--- server.js
+++ server.js
@@ -24,17 +24,33 @@ app.get('/new/:url(*)', function(req, res){
 	  if (err) {
 	    console.log("Unable to connect to server", err);
 	  } else {
-	    console.log("Connected to server")
-	    // var collection = db.collection('links');
-	    // var params = req.params.url;
-	    // var newLink = function(db, callback) {
-	    // 	newLink(db, function() {
-	    // 		var insertLink = {url: params, short: "test" };
-	    // 		collection.insert([insertLink]);
-	    // 		res.send(params);
-	    // 		db.close();
-	    // 	});
-	    // }
+	    console.log("Connected to server");
+	    var collection = db.collection('links');
+	    var params = req.params.url;
+	    var newLink = function(db, callback) {
+	    	if (validUrl.isUri(params)) {
+	   		// do stuff
+	   		var shortCode = shortid.generate();
+	   		var newUrl = { url: params, short: shortCode };
+	   		collection.insert([newUrl]);
+	   		res.json({
+	   			original_url: params,
+	   			short_url: "http://localhost:8080/" + shortCode
+	   		});
+	   	} else {
+	   		// do other stuff
+	   		res.json({
+	   			error: "Wrong URL format. Make sure it's a real site and has valid http/https protocol!"
+	   		});
+	   	}
+	  //   	var insertLink = { url: params, short: "test" };
+			// collection.insert([insertLink]);
+			// res.send(params);
+	   };
+	   newLink(db, function() {
+
+	   	db.close();
+	   });
 	  };
 	});
 });
